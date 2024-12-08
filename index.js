d43d61ff7e835523017cece246ba61c6c0777d54
zoranmil <milicevic.zoran@gmail.com>
2024-12-08 12:21:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+/// npm install sqlite3  --target_platform=win32   --runtime=node-webkit --target_arch=ia32
+const { app, BrowserView, BrowserWindow, Menu,Tray } = require('electron')
+const path = require("path");
+
+let mainWindow;
+
+const omenuTemplate = [
+  {
+    label: 'Program',
+    submenu: [{   role: 'quit',label: 'Izlaz' }],
+  }
+];
+async function createAboutWindow () {
+  childWindow = new BrowserWindow({
+    height: 400,
+    width: 600,
+    show: false,
+    minimizable: false,
+    maximizable: false,
+    parent: mainWindow,
+    icon: __dirname + '/assets/favicon.ico',
+    webPreferences: {
+      contextIsolation: false,
+       nodeIntegration: true,
+       enableRemoteModule: true,
+       webSecurity: false,
+       allowRunningInsecureContent: false,
+    }
+  });
+ childWindow.setIcon(path.join(__dirname, '/assets/favicon.ico'));
+
+  childWindow.removeMenu();
+
+  childWindow.loadURL(`file://${__dirname}/about.html`);
+  childWindow.webContents.on('dom-ready', () => {
+    childWindow.show();
+  });
+}
+
+app.on("ready", () => {
+    app.allowRendererProcessReuse = false;
+    mainWindow = new BrowserWindow({
+        width: 1200, height: 800,
+         icon: __dirname + 'assets/favicon.ico',
+        webPreferences: {
+          contextIsolation: false,
+           nodeIntegration: true,
+
+        },
+    });
+    mainWindow.loadFile(path.join(__dirname,  "index.html"));
+   mainWindow.setIcon(path.join(__dirname, '/assets/favicon.ico'));
+   const mainMenu = Menu.buildFromTemplate(omenuTemplate);
+     Menu.setApplicationMenu(mainMenu);
+      const tMenu = Menu.buildFromTemplate(omenuTemplate);
+    tray = new Tray( __dirname + '/assets/favicon.ico')
+    tray.setToolTip('vtr_vasa.')
+ tray.setContextMenu(tMenu)
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+    // Emitted when the window is closed.
+    mainWindow.on("closed", function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
---
+++ renderer.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Db = require('./node_modules/db/db.js');
+const JSON_DBPATH="baza";
+
+
+const DB=new Db( JSON_DBPATH);
